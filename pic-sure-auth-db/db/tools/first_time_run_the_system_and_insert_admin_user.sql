e3cf23f45f81b28afd99e9cd661ed5e5ec9a8448
Yu Zhang <yu_zhang@hms.harvard.edu>
2019-04-01 13:23:39 -0400

    modify the first init admin user script

--- pic-sure-auth-db/db/tools/first_time_run_the_system_and_insert_admin_user.sql
+++ pic-sure-auth-db/db/tools/first_time_run_the_system_and_insert_admin_user.sql
@@ -1,25 +1,25 @@
 #configuration for user table
 #for quick usage, please use a google user
 #otherwise you need to modify connection configuration as well
-set @user_uuid='6dc35d47c7c64ca7beb42edac5184e10';
+set @user_uuid=REPLACE(uuid(),'-','');
 set @user_email="change_me_to_your_email";
 set @matched=false;
 
 #configuration for connection table
-set @connection_uuid=0x6dc35d47c7c64ca7beb42edac5184e10;
+set @connection_uuid=REPLACE(uuid(),'-','');
 set @label='Google';
 set @id='google-oauth2';
 set @subprefix='google-oauth2|';
 set @requiredFields='[{\"label\":\"Email\", \"id\":\"email\"}]';
 
 #configuration for userMetadataMapping table
-set @userMetadataMapping_uuid=0xed5c801a73ef4a7281de07363bb5cdba;
+set @userMetadataMapping_uuid=REPLACE(uuid(),'-','');
 set @authMetadata='$.email';
 set @generalMetadata='$.email';
 
 begin;
 #### insert into connection table
-insert into `connection` values (@connection_uuid, @label, @id, @subprefix, @requiredFields);
+insert into `connection` values (unhex(@connection_uuid), @label, @id, @subprefix, @requiredFields);
 
 #### insert into user table
 create or replace view management_view as
@@ -32,11 +32,11 @@ where
 role.uuid = role_privilege.role_id
 and privilege.uuid = role_privilege.privilege_id;
 
-insert into user (`uuid`,`general_metadata`,`connectionId`,`matched`) values (unhex(@user_uuid), CONCAT("{\"email\":\"", @user_email, "\"}"), @connection_uuid, @matched);
+insert into user (`uuid`,`general_metadata`,`connectionId`,`email`,`matched`) values (unhex(@user_uuid), CONCAT("{\"email\":\"", @user_email, "\"}"), unhex(@connection_uuid), @user_email, @matched);
 insert into user_role (`user_id`,`role_id`) values (unhex(@user_uuid),
 (select min(uuid) from management_view where privilege_name like 'ADMIN')) ;
 
 #### insert into userMetadataMapping
-insert into `userMetadataMapping` values (@userMetadataMapping_uuid, @authMetadata, @connection_uuid, @generalMetadata);
+insert into `userMetadataMapping` values (unhex(@userMetadataMapping_uuid), @authMetadata, unhex(@connection_uuid), @generalMetadata);
 commit;
 
